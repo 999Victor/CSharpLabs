4d3f299764dea7975e0fa3338a1f34b5ba7c5c66
Виктор <successfulvictoragb@gmail.com>
2020-04-27 13:35:09 +0300

    Update Comparer.cs

--- Comparer.cs
+++ Comparer.cs
@@ -6,14 +6,17 @@ namespace Lab7
     {
         public int Compare(RationalNum obj1, RationalNum obj2)
         {
-            if ((obj1.FirstNum == obj2.FirstNum) && (obj1.SecondNum == obj2.SecondNum))
+            if (obj1._firstNum * obj2._secondNum > obj2._firstNum * obj1._secondNum)
             {
                 return 1;
             }
+            else if ((obj1._firstNum == obj2._firstNum) && (obj1._secondNum == obj2._secondNum))
+            {
+                return 0;
+            }
             else
             {
-                return (obj1.FirstNum * obj2.SecondNum > obj2.FirstNum * obj1.SecondNum) ? 
-                    (obj1.FirstNum / obj1.SecondNum) : (obj2.FirstNum / obj2.SecondNum);
+                return -1;
             }
         }
     }
